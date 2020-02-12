cc51fa5d3113e75770e07ff07b6721df89f52cc0
Simao <lucas.simao@epitech.eu>
2020-02-12 17:41:06 +0100

    [ADD] src/strlen.asm / push the strlen function.

---
+++ src/strlen.asm
@@ -0,0 +1,22 @@
+;
+; EPITECH PROJECT, 2020
+; ASM_minilibc_2019
+; File description:
+; ASM
+;
+
+BITS 64
+
+section .text
+    global strlen
+
+        strlen:
+            mov rax, rdi
+            .start:
+            cmp BYTE [rax], 0x0
+            jz short .end
+            inc rax
+            jmp short .start
+            .end:
+            sub rax, rdi
+            ret
