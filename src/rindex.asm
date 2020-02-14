37c5600ff8904547f2460dd705e21e86c9fadd59
Lucas Simao <lucas.simao@epitech.eu>
2020-02-14 18:59:39 +0100

    [ADD] rindex.asm / add rindex

---
+++ src/rindex.asm
@@ -0,0 +1,28 @@
+;
+; EPITECH PROJECT, 2020
+; ASM_minilibc_2019
+; File description:
+; ASM
+;
+
+BITS 64
+
+section .text
+    global rindex
+        rindex:
+
+            .start:
+            cmp BYTE [rdi], 0x0
+            jz short .end
+            cmp BYTE [rdi], sil
+            jz short .occ
+            inc rdi
+            jmp .start
+
+            .occ:
+            mov rax, rdi
+            inc rdi
+            jmp .start
+
+            .end:
+            ret
