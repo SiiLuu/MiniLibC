af78f4691660e8c878a28accfed46f5bc80aa6bf
Lucas <lucas.simao@epitech.eu>
2020-02-16 18:53:36 +0100

    [ADD] strcasecmp

---
+++ src/strcasecmp.asm
@@ -0,0 +1,68 @@
+;
+; EPITECH PROJECT, 2020
+; ASM_minilibc_2019
+; File description:
+; ASM
+;
+
+BITS 64
+
+section .text
+    global strcasecmp
+
+        strcasecmp:
+            xor cl, cl
+            xor dl, dl
+
+            .start:
+            cmp BYTE [rdi], 0x0
+            jz short .verifsec
+            cmp BYTE [rsi], 0x0
+            jz short .veriffirst
+
+            add cl, BYTE [rdi]
+            add dl, BYTE [rsi]
+            cmp cl, dl
+            jne short .up
+            inc rdi
+            inc rsi
+            jmp short .start
+            
+            .up:
+            add dl, 32
+            cmp cl, dl
+            jne short .lower
+            inc rdi
+            inc rsi
+            jmp short .start
+
+            .lower:
+            sub dl, 32
+            add cl, 32
+            cmp cl, dl
+            jne short .not
+            inc rdi
+            inc rsi
+            jmp short .start
+
+            .not:
+            add dl, 32
+            sub cl, 32
+            jmp short .end
+
+            .verifsec:
+            cmp BYTE [rsi], 0x0
+            jz short .end
+            sub dl, BYTE [rsi]
+            jmp short .end
+
+            .veriffirst:
+            cmp BYTE [rdi], 0x0
+            jz short .end
+            sub dl, BYTE [rdi]
+            jmp short .end
+
+            .end:
+            sub cl, dl
+            movsx rax, cl
+            ret
