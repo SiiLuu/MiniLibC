bcf95cbcb714e5360c07ceef4ca54364c8ba264f
Simao <lucas.simao@epitech.eu>
2020-02-17 13:04:54 +0100

    [ADD] strstr.asm

--- src/strstr.asm
+++ src/strstr.asm
@@ -11,5 +11,36 @@ section .text
     global strstr
 
         strstr:
+            xor dl, dl
+            xor rcx, rcx
 
-        ret
+            .start:
+            cmp BYTE [rdi], 0x0
+            jz short .end
+            mov dl, BYTE [rsi]
+            cmp BYTE [rdi], dl
+            jz short .check
+            inc rdi
+            jmp .start
+
+            .check:
+            inc rcx
+            inc rdi
+            inc rsi
+            mov dl, BYTE [rsi]
+            cmp BYTE [rdi], dl
+            jz short .check
+            cmp BYTE [rsi], 0x0 
+            jz short .return
+            jmp short .start
+
+            .return:
+            cmp rcx, 0
+            jz short .end
+            dec rcx
+            dec rdi
+            jmp short .return
+
+            .end:
+            mov rax, rdi
+            ret
