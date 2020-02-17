8bdfd51f19fb155464c137b98e693d7bcba1df3d
Simao <lucas.simao@epitech.eu>
2020-02-17 10:18:09 +0100

    [MODIF] strcspn.

--- src/strcspn.asm
+++ src/strcspn.asm
@@ -11,26 +11,26 @@ section .text
     global strcspn
 
         strcspn:
-            xor dl, dl
-            mov rax, rsi
-
-            .start:
-            cmp BYTE [rax], 0x0
-            jz short .incr
-            cmp BYTE [rdi], 0x0
-            jz short .notfind
-            mov dl, BYTE [rdi]
-            cmp BYTE [rax], dl
-            jz short .find
-            inc rax
-            jmp .start
-
-            .incr:
-            xor rax, rax
-            mov rax, rsi
-            inc rdi
-            jmp short .start
-
-            .end:
-            sub rax, rdi
-            ret
+            ;xor dl, dl
+            ;mov rax, rsi
+;
+            ;.start:
+            ;cmp BYTE [rax], 0x0
+            ;jz short .incr
+            ;cmp BYTE [rdi], 0x0
+            ;jz short .notfind
+            ;mov dl, BYTE [rdi]
+            ;cmp BYTE [rax], dl
+            ;jz short .find
+            ;inc rax
+            ;jmp .start
+;
+            ;.incr:
+            ;xor rax, rax
+            ;mov rax, rsi
+            ;inc rdi
+            ;jmp short .start
+;
+            ;.end:
+            ;sub rax, rdi
+            ;ret
